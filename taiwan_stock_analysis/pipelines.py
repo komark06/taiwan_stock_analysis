45daf5fbe8f7008ecae228bf389a5cefaf307026
komark06 <komark06@gmail.com>
2023-10-30 18:14:18 +0800

    Refactor: Database Pipelines
    Remove the 'STOCK_INFO_OVERWRITE' setting. If a table already exists, it
    will attempt to rename the table by appending the '_old' suffix.
    
    The value of 'data_type'now accepts a Python tuple with multiple
    statements.
    
    Introduce the 'primary_key' variable to set the primary key of the table.

--- taiwan_stock_analysis/pipelines.py
+++ taiwan_stock_analysis/pipelines.py
@@ -2,137 +2,96 @@ import logging
 import os
 import sqlite3
 
-from scrapy.exceptions import NotConfigured
-
 
 class StockInfoPipeline:
     data_type = {
-        "classification": "TEXT",
-        "symbol": "TEXT",
-        "name": "TEXT",
-        "ISINCode": "TEXT",
-        "listing_date": "TEXT",
-        "market_category": "TEXT",
-        "industry_category": "TEXT",
-        "CFICode": "TEXT",
-        "remark": "TEXT",
+        "classification": ("TEXT",),
+        "symbol": ("TEXT",),
+        "name": ("TEXT",),
+        "ISINCode": ("TEXT",),
+        "listing_date": ("TEXT",),
+        "market_category": ("TEXT",),
+        "industry_category": ("TEXT",),
+        "CFICode": ("TEXT",),
+        "remark": ("TEXT",),
     }
+    primary_key = ("symbol", "name")
 
     def __init__(
         self,
-        overwrite: bool = False,
     ):
-        self.over_write = overwrite
         self.table_name = "stock_info"
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    @classmethod
-    def from_crawler(
-        cls,
-        crawler,
-    ):
-        setting_name = "STOCK_INFO_OVERWRITE"
-        overwrite = crawler.settings.get(setting_name)
-        logger = logging.getLogger(cls.__name__)
-        if type(overwrite) is not bool:
-            logger.warning(
-                f"{setting_name} must be Boolean value. "
-                f"Set {setting_name} to False by default."
-            )
-            overwrite = False
-        return cls(overwrite)
-
-    def open_spider(
-        self,
-        spider,
-    ):
+    def open_spider(self, spider):
         output_folder_name = "output"
         db_name = "stock_info.db"
-
         current_folder = os.path.dirname(os.path.abspath(__file__))
-        output_path = os.path.join(
-            current_folder,
-            output_folder_name,
-        )
-        os.makedirs(
-            output_path,
-            exist_ok=True,
-        )
-        self.db_path = os.path.join(
-            output_path,
-            db_name,
-        )
+        output_path = os.path.join(current_folder, output_folder_name)
+        os.makedirs(output_path, exist_ok=True)
+        self.db_path = os.path.join(output_path, db_name)
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
-        if self._check_table() is False:
-            if self.over_write is False:
-                raise NotConfigured(
-                    f"Structure of table is incorrect and "
-                    "overwrite is set to False: "
-                    f"table name is {self.table_name}"
-                )
-            self.cursor.execute(f"DROP TABLE IF EXISTS {self.table_name}")
-            columns = []
-            for (
-                column_name,
-                column_type,
-            ) in self.data_type.items():
-                if column_type is not None:
-                    columns.append(f"{column_name} {column_type}")
-                else:
-                    columns.append(column_name)
-            columns.append("PRIMARY KEY (symbol, name)")
-            self.cursor.execute(
-                f"CREATE TABLE {self.table_name} ({', '.join(columns)})"
-            )
-            self.connection.commit()
+        self.create_table_query = self._create_table_command()
+        self._create_table()
 
-    def _check_table(
-        self,
-    ) -> bool:
+    def _create_table(self):
         """
         Check if a structure of table is as same as setting.
-        """
-        self.cursor.execute(f"PRAGMA table_info({self.table_name})")
-        table = self.cursor.fetchall()
-        if len(table) == 0:
-            return False
-        for column in table:
-            name = column[1]
-            expect_data_type = column[2]
-            data_type = self.data_type.get(name)
-            if data_type is None:
-                return False
-            elif data_type != expect_data_type:
-                return False
-        return True
 
-    def add(
-        self,
-        *data,
-    ):
+        If table is not existed, create a new one.
+        If structure of table is incorrect, rename it by adding '_old'
+        suffix and then create a new one.
+        """
+        self.cursor.execute(
+            "SELECT sql FROM sqlite_master WHERE tbl_name = "
+            f"'{self.table_name}'"
+        )
+        data = self.cursor.fetchall()
+        if len(data) == 0:
+            self.cursor.execute(self.create_table_query)
+            return
+        query = data[0][0]
+        if query != self.create_table_query:
+            new_table_name = self.table_name + "_old"
+            self.logger.info(
+                f"Structure of {self.table_name} is incorrect."
+                "Rename it to {new_table_name} and create a new one."
+            )
+            self.cursor.execute(
+                f"ALTER TABLE {self.table_name} RENAME TO {new_table_name}"
+            )
+            self.cursor.execute(self.create_table_query)
+
+    def _create_table_command(self):
+        """
+        Return command to create table.
+        """
+        columns = []
+        for name, statement in self.data_type.items():
+            column = [name]
+            if statement:
+                column.append(*statement)
+            columns.append(" ".join(column))
+        if self.primary_key:
+            columns.append(f"PRIMARY KEY ({', '.join(self.primary_key)})")
+        return f"CREATE TABLE {self.table_name} ({', '.join(columns)})"
+
+    def add(self, *data):
         if not data:
             return
         if len(data) != len(self.data_type):
             raise ValueError("Data items do not match table columns.")
         placeholders = ", ".join(["?" for _ in data])
-        insert_sql = f"INSERT INTO {self.table_name} VALUES ({placeholders})"
-        self.cursor.execute(
-            insert_sql,
-            data,
+        insert_sql = (
+            f"INSERT OR REPLACE INTO {self.table_name} VALUES ({placeholders})"
         )
+        self.cursor.execute(insert_sql, data)
 
-    def close_spider(
-        self,
-        spider,
-    ):
+    def close_spider(self, spider):
         self.connection.commit()
         self.connection.close()
 
-    def process_item(
-        self,
-        item,
-        spider,
-    ):
+    def process_item(self, item, spider):
         self.add(*item.values())
         return item

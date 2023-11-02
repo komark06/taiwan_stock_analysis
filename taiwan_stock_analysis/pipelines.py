72a895ddfd3a3cda31a239846e49ea0498ef4a4c
komark06 <komark06@gmail.com>
2023-11-02 20:30:33 +0800

    Refactor pipelines add daily_trading.py
    Enhanced the StockInfoPipeline by incorporating a class variable
    for easy access across components.
    Implemented the DailyTradingPipeline, specifically tailored to
    process data obtained from the DailyTradingSpider.
    Within the DailyTradingPipeline, two SQLite tables were introduced:
    one dedicated to storing the data and the other to verify the
    completion status of monthly data.
    
    daily_trading.py:
        Introduced the DailyTradingSpider, designed to extract daily trading
        information from the Taiwan Stock Exchange. This process involves
        gathering stock information via the StockInfoSpider, followed by
        sending requests based on listing dates and assessing the completion
        status of monthly data. Each request facilitates the extraction of
        an entire month's worth of data.

--- taiwan_stock_analysis/pipelines.py
+++ taiwan_stock_analysis/pipelines.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sqlite3
+from datetime import datetime, timedelta
 
 
 class StockInfoPipeline:
@@ -16,23 +17,19 @@ class StockInfoPipeline:
         "remark": ("TEXT",),
     }
     primary_key = ("symbol", "name")
-
-    def __init__(
-        self,
-    ):
-        self.table_name = "stock_info"
-        self.logger = logging.getLogger(self.__class__.__name__)
+    output_folder_name = "output"
+    db_name = "stock_info.db"
+    table_name = "stock_info"
 
     def open_spider(self, spider):
-        output_folder_name = "output"
-        db_name = "stock_info.db"
         current_folder = os.path.dirname(os.path.abspath(__file__))
-        output_path = os.path.join(current_folder, output_folder_name)
+        output_path = os.path.join(current_folder, self.output_folder_name)
         os.makedirs(output_path, exist_ok=True)
-        self.db_path = os.path.join(output_path, db_name)
+        self.db_path = os.path.join(output_path, self.db_name)
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
         self.create_table_query = self._create_table_command()
+        self.logger = logging.getLogger(self.__class__.__name__)
         self._create_table()
 
     def _create_table(self):
@@ -56,7 +53,7 @@ class StockInfoPipeline:
             new_table_name = self.table_name + "_old"
             self.logger.info(
                 f"Structure of {self.table_name} is incorrect."
-                "Rename it to {new_table_name} and create a new one."
+                f"Rename it to {new_table_name} and create a new one."
             )
             self.cursor.execute(
                 f"ALTER TABLE {self.table_name} RENAME TO {new_table_name}"
@@ -94,4 +91,136 @@ class StockInfoPipeline:
 
     def process_item(self, item, spider):
         self.add(*item.values())
+        return item
+
+
+class DailyTradingPipeline:
+    data_type = {
+        "symbol": ("INTEGER",),
+        "year": ("INTEGER",),
+        "month": ("INTEGER",),
+        "day": ("INTEGER",),
+        "volume": ("INTEGER",),
+        "value": ("INTEGER",),
+        "open": ("REAL",),
+        "highest": ("REAL",),
+        "lowest": ("REAL",),
+        "closing": ("REAL",),
+        "change": ("TEXT",),
+        "transaction_volume": ("INTEGER",),
+    }
+    primary_key = ("symbol", "year", "month", "day")
+    table_name = "stock_daily_trading"
+    check_table_name = "check_table"
+    output_folder_name = "output"
+    db_name = "stock_daily.db"
+
+    def open_spider(self, spider):
+        current_folder = os.path.dirname(os.path.abspath(__file__))
+        output_path = os.path.join(current_folder, self.output_folder_name)
+        os.makedirs(output_path, exist_ok=True)
+        self.db_path = os.path.join(output_path, self.db_name)
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+        self.create_table_query = self._create_table_command()
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self._create_table()
+
+    def _create_table(self):
+        """
+        Create table if needed.
+
+        If table is not existed, create a new one.
+        If structure of table is incorrect, rename it by adding '_old'
+        suffix and then create a new one.
+        """
+        self.cursor.execute(
+            "SELECT sql FROM sqlite_master WHERE tbl_name = "
+            f"'{self.table_name}'"
+        )
+        data = self.cursor.fetchall()
+        if len(data) == 0:
+            self.cursor.execute(self.create_table_query)
+            return
+        query = data[0][0]
+        if query != self.create_table_query:
+            new_table_name = self.table_name + "_old"
+            self.logger.info(
+                f"Structure of {self.table_name} is incorrect."
+                f"Rename it to {new_table_name} and create a new one."
+            )
+            self.cursor.execute(
+                f"ALTER TABLE {self.table_name} RENAME TO {new_table_name}"
+            )
+            self.cursor.execute(self.create_table_query)
+        s = (
+            f"CREATE TABLE IF NOT EXISTS {self.check_table_name} "
+            "(symbol INTEGER, year INTEGER, month INTEGER, update_time TEXT)"
+        )
+        self.cursor.execute(s)
+
+    def _create_table_command(self):
+        """
+        Return command to create table.
+        """
+        columns = []
+        for name, statement in self.data_type.items():
+            column = [name]
+            if statement:
+                column.append(*statement)
+            columns.append(" ".join(column))
+        if self.primary_key:
+            columns.append(f"PRIMARY KEY ({', '.join(self.primary_key)})")
+        return f"CREATE TABLE {self.table_name} ({', '.join(columns)})"
+
+    def add(self, *data):
+        if not data:
+            return
+        if len(data) != len(self.data_type):
+            raise ValueError("Data items do not match table columns.")
+        placeholders = ", ".join(["?" for _ in data])
+        insert_sql = (
+            f"INSERT OR REPLACE INTO {self.table_name} VALUES ({placeholders})"
+        )
+        self.cursor.execute(insert_sql, data)
+
+    def commit(self):
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.commit()
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        symbol = item["symbol"]
+        data_list = item["data"]
+        fields = item["fields"]
+        for data in data_list:
+            date = data[0]
+            formatted_data = {"symbol": symbol}
+            formatted_data.update(
+                (name, value)
+                for name, value in zip(
+                    ["year", "month", "day"], date.split("/")
+                )
+            )
+            for name, value in zip(fields[1:], data[1:]):
+                formatted_data[name] = value.replace(",", "")
+            self.add(*formatted_data.values())
+        current = datetime.utcnow() + timedelta(hours=8)  # Current taipei time
+        year = int(formatted_data["year"])
+        month = int(formatted_data["month"])
+        if current.year != year or current.month != month:
+            self.cursor.execute(
+                f"INSERT OR REPLACE INTO {self.check_table_name} "
+                "(symbol, year, month, update_time) VALUES (?, ?, ?, ?)",
+                (symbol, year, month, current.strftime("%Y/%m/%d/%H:%M")),
+            )
+            self.logger.info(f"{symbol} {year}/{month} is done.")
+        else:
+            self.logger.info(
+                f"{symbol} {year}/{month} is current month, "
+                "so we need to update everyday."
+            )
+        self.commit()
         return item
---
+++ taiwan_stock_analysis/spiders/daily_trading.py
@@ -0,0 +1,181 @@
+import json
+import os
+import random
+import re
+import sqlite3
+from datetime import datetime, timedelta
+from typing import Any
+
+from scrapy import Request, Spider
+from scrapy.http import Response
+
+from ..pipelines import DailyTradingPipeline, StockInfoPipeline
+
+DailyTradingPipeline
+
+
+class DailyTradingSpider(Spider):
+    name = "test"
+    custom_settings = {
+        "DOWNLOAD_DELAY": 4,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 1,
+        "ITEM_PIPELINES": {
+            "taiwan_stock_analysis.pipelines.DailyTradingPipeline": 300,
+        },
+        "ROBOTSTXT_OBEY": False,
+        "TELNETCONSOLE_USERNAME": "komark",
+        "TELNETCONSOLE_PASSWORD": "123",
+    }
+    base_url = "https://www.twse.com.tw/rwd/en/afterTrading/STOCK_DAY"
+
+    def __init__(self, name: str | None = None, **kwargs: Any):
+        super().__init__(name, **kwargs)
+        upper_folder = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))
+        )
+        db_folder = DailyTradingPipeline.output_folder_name
+        db_name = DailyTradingPipeline.db_name
+        db_path = os.path.join(upper_folder, db_folder, db_name)
+        if os.path.exists(db_path):
+            url_path = f"file:{db_path}?mode=ro"
+            table = DailyTradingPipeline.check_table_name
+            con = sqlite3.connect(url_path)
+            cur = con.execute(
+                "SELECT name FROM sqlite_master WHERE name = ?", (table,)
+            )
+            if not cur.fetchone():
+                con.close()
+                self.connection = None
+            else:
+                self.connection = con
+        else:
+            self.connection = None
+
+    def start_requests(self):
+        def get_symbols():
+            """
+            Get all informations of symbols.
+            """
+            upper_folder = os.path.dirname(
+                os.path.dirname(os.path.abspath(__file__))
+            )
+            db_folder = StockInfoPipeline.output_folder_name
+            db_name = StockInfoPipeline.db_name
+            db_path = os.path.join(upper_folder, db_folder, db_name)
+            url_path = f"file:{db_path}?mode=ro"
+            table = StockInfoPipeline.table_name
+            con = sqlite3.connect(url_path)
+            symbols = [
+                (int(item[0]), item[1])
+                for item in con.execute(
+                    f"SELECT symbol,listing_date FROM {table} "
+                    "WHERE classification='股票'"
+                )
+            ]
+            con.close()
+            return symbols
+
+        def next_month(year: int, month: int):
+            """
+            Return date of next month.
+            """
+            if month == 12:
+                year += 1
+                month = 1
+            else:
+                month += 1
+            return year, month
+
+        def exist(year, month, symbol):
+            """
+            Check if data exist or not.
+
+            If exist, return True. Else, return False.
+            """
+            table = DailyTradingPipeline.check_table_name
+            data = {"year": year, "month": month, "symbol": symbol}
+            if (
+                self.connection
+                and self.connection.execute(
+                    f"SELECT * from {table} WHERE year=':year' AND "
+                    "month=':month' AND symbol=':symbol'",
+                    data,
+                ).fetchone()
+            ):
+                return True
+            return False
+
+        symbols = get_symbols()
+        start_year = 2010
+        start_month = 1
+        for item in random.sample(symbols, len(symbols)):
+            symbol = item[0]
+            listing_date = item[1]
+            year, month, day = listing_date.split("/")
+            year = int(year)
+            month = int(month)
+            day = int(day)
+            current = datetime.utcnow() + timedelta(
+                hours=8
+            )  # Current taipei time
+            """
+            Begin from the listing date if the listing date is more
+            recent than the specified start date.
+            """
+            if year < start_year:
+                year = start_year
+                month = start_month
+            end_year, end_month = next_month(current.year, current.month)
+            while end_year != year or end_month != month:
+                if not exist(year, month, symbol):
+                    yield Request(
+                        url=self.generate_url(year, month, symbol),
+                        callback=self.parse,
+                        cb_kwargs={
+                            "year": year,
+                            "month": month,
+                            "request_symbol": symbol,
+                        },
+                    )
+                else:
+                    self.logger.info(
+                        f"{symbol} {year}/{month} already exists. Pass it."
+                    )
+                year, month = next_month(year, month)
+
+    def generate_date(self, year: int, month: int):
+        return f"{year}{month:02d}01"
+
+    def generate_url(self, year: int, month: int, symbol: int):
+        date_str = self.generate_date(year, month)
+        return (
+            f"{self.base_url}?date={date_str}&stockNo={symbol}&response=json"
+        )
+
+    def parse(
+        self,
+        response: Response,
+        year: int,
+        month: int,
+        request_symbol: int,
+    ):
+        js = json.loads(response.text)
+        if js["stat"] != "OK":
+            self.logger.warning(f"{response.url}:{response.text}")
+            return
+        if self.generate_date(year, month) != js["date"]:
+            self.logger.warning(
+                f"Wrong date. Request_symbol:{request_symbol}, year:{year}"
+                f", month:{month}, url: {response.url}"
+            )
+            return
+        title = js["title"].strip()
+        symbol = int(re.split(r"\s+", title)[-1])
+        if request_symbol != symbol:
+            self.logger.warning(
+                f"Wrong symbol. Request_symbol:{request_symbol}, "
+                f"symbol:{symbol}, year:{year}, month:{month}"
+                f", url: {response.url}"
+            )
+            return
+        yield {"symbol": symbol, "fields": js["fields"], "data": js["data"]}

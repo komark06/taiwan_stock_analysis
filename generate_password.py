da0e0cd86859c40e7e1ba6784462912a4ac05e89
komark06 <komark06@gmail.com>
2023-12-05 17:44:47 +0800

    Change password strategy
    Remove hard-coded password and use python script to generate password.
    Add user account to MariaDB for future use.

---
+++ generate_password.py
@@ -0,0 +1,22 @@
+import os
+import secrets
+import string
+
+
+def generate_password(min_length, max_length):
+    length = secrets.randbelow(max_length - min_length + 1) + min_length
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(characters) for _ in range(length))
+    return password
+
+
+folder = "db"
+if not (os.path.exists(folder) and os.path.isdir(folder)):
+    os.mkdir(folder)
+min_length = 8  # Minimum length of the password
+max_length = 16  # Maximum length of the password
+password_file = ["password.txt", "user-password.txt"]
+for file in password_file:
+    path = os.path.join(folder, file)
+    with open(path, "w", encoding="ascii") as fw:
+        fw.write(generate_password(min_length, max_length))

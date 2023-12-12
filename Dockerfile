199bcd8c15560e28fdcff7d646151cd6e42df03b
komark06 <komark06@gmail.com>
2023-12-12 10:38:00 +0800

    Add start.sh for Database Initialization
    Introduce a start.sh script responsible for initializing the database
    created by the backup service. The script will subsequently launch the
    Scrapy process.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,21 @@
 FROM python:3.12-slim
 
-WORKDIR /app
-
-COPY . /app/
-
 RUN apt-get update && apt-get install -y --no-install-recommends \
     libmariadb3 \
     libmariadb-dev \
     gcc \
+    mariadb-client \
     && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY . /app/
 
 RUN pip install --upgrade pip && \
     pip install pipenv && \
     pipenv install --deploy
 
-CMD ["pipenv", "run", "python3", "run.py"]
+RUN chmod +x start.sh
+
+CMD ["/bin/bash", "/app/start.sh"]
 
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Program:
+#       If init.sql exists, we copy it into mariadb.
+
+sql_file="/data/backup.sql"
+if [ ! -e "$sql_file" ]; then
+    echo "The file $sql_file does not exist."
+else
+    date="$(date '+%Y-%m-%d %H:%M:%S')"
+    echo "$date: $sql_file exists. Start copy to mariaDB."
+    time mariadb --host=db --user=root --password=$(cat /run/secrets/db-password) -D example < $sql_file
+    date="$(date '+%Y-%m-%d %H:%M:%S')"
+    if [ $? -eq 0 ]; then
+        echo "$date :Init succeed."
+    else
+        echo "$date :Init FAIL!"
+        exit 1
+    fi
+fi
+
+pipenv run python3 run.py

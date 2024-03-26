74fd08e6f90874f3d3f6cfc17bdf0afeaf6b52a4
komark06 <komark06@gmail.com>
2024-03-26 18:26:57 +0800

    Refactor: Utilize SQLAlchemy for MariaDB Operation
    Instead of reinventing the wheel, import SQLAlchemy for efficient
    operations with MariaDB. This commit integrates SQLAlchemy, enhancing
    database interaction and promoting code maintainability and
    scalability.

--- taiwan_stock_analysis/pipelines.py
+++ taiwan_stock_analysis/pipelines.py
@@ -1,73 +1,136 @@
+import datetime
 import logging
 import os
-from datetime import datetime, timedelta
+from typing import Optional
 
-from taiwan_stock_analysis.database.datatype import (
-    UNSIGNED,
-    BigInt,
-    Decimal,
+import scrapy
+import sqlalchemy
+from sqlalchemy import (
+    Date,
+    DateTime,
+    Double,
     Integer,
-    SmallInt,
-    Text,
-    TinyInt,
-    VarChar,
+    SmallInteger,
+    String,
+    create_engine,
+)
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    MappedAsDataclass,
+    Session,
+    mapped_column,
 )
 
-from .database.mariadbclient import MariadbClient, MariadbTable
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
 
 
-def login_info() -> dict:
-    """Return login info of Mariadb"""
-    info = {"user": "root"}
+class StockInfo(Base):
+    __tablename__ = "stock_info"
+    classification: Mapped[str] = mapped_column(String(50))
+    symbol: Mapped[str] = mapped_column(String(25))
+    name: Mapped[str] = mapped_column(String(50))
+    ISINCode: Mapped[str] = mapped_column(String(25), primary_key=True)
+    listing_date: Mapped[datetime.date] = mapped_column(Date)
+    market_category: Mapped[str] = mapped_column(String(50))
+    industry_category: Mapped[Optional[str]] = mapped_column(
+        String(50), nullable=True
+    )
+    CFICode: Mapped[str] = mapped_column(String(25))
+    remark: Mapped[Optional[str]] = mapped_column(String(25), nullable=True)
+
+    def update_attributes(self, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+
+
+class DailyTradingRecord(Base):
+    __tablename__ = "stock_daily_trading_info_record"
+    symbol: Mapped[int] = mapped_column(SmallInteger, primary_key=True)
+    timestamp: Mapped[datetime.date] = mapped_column(Date, primary_key=True)
+    update_time: Mapped[datetime.datetime] = mapped_column(DateTime)
+
+
+class DailyTradingInfo(Base):
+    __tablename__ = "stock_daily_trading_info"
+    symbol: Mapped[int] = mapped_column(SmallInteger, primary_key=True)
+    timestamp: Mapped[str] = mapped_column(Date, primary_key=True)
+    volume: Mapped[int] = mapped_column(Integer)
+    value: Mapped[int] = mapped_column(Integer)
+    open: Mapped[float] = mapped_column(Double)
+    highest: Mapped[float] = mapped_column(Double)
+    lowest: Mapped[float] = mapped_column(Double)
+    closing: Mapped[float] = mapped_column(Double)
+    delta: Mapped[float] = mapped_column(String(10))
+    transaction_volume: Mapped[int] = mapped_column(Integer)
+    attr_name = [
+        "symbol",
+        "timestamp",
+        "volume",
+        "value",
+        "open",
+        "highest",
+        "closing",
+        "delta",
+        "transaction_volume",
+    ]
+
+    def update_attributes(self, obj):
+        for name in self.attr_name:
+            setattr(self, name, getattr(obj, name))
+
+
+def init_engine(echo: bool = False) -> sqlalchemy.engine.Engine:
+    """
+    Initialize the SQLAlchemy engine and return it.
+
+    Parameters:
+        echo (bool): A flag indicating whether to enable echo mode.
+                     When set to True, SQL statements will be printed
+                     to the console.
+                     Defaults to False.
+
+    Returns:
+        sqlalchemy.engine.Engine: The SQLAlchemy engine instance.
+    """
     password_file = os.getenv("MARIADB_ROOT_PASSWORD_FILE")
-    if password_file:
-        with open(password_file, "r") as file:
-            info["password"] = file.read()
-    else:
-        info["password"] = os.getenv("MARIADB_ROOT_PASSWORD")
-    info["host"] = os.getenv("MARIADB_HOST")
-    info["database"] = os.getenv("MARIADB_DATABASE")
-    return info
+    with open(password_file, "r") as file:
+        password = file.read()
+    host = os.getenv("MARIADB_HOST")
+    database = os.getenv("MARIADB_DATABASE")
+    url = (
+        "mariadb+mariadbconnector://root:"
+        f"{password}@{host}/{database}?charset=utf8mb4"
+    )
+    return create_engine(
+        url,
+        pool_recycle=3600,
+        echo=echo,
+    )
 
 
 class StockInfoPipeline:
     """
     A class for processing information of stock.
-
-    Attributes:
-        data_type (List[DateType]): A list composed of DataType used to
-        represent a SQL table.
-        table_name (str): The name of SQL table.
     """
 
-    data_type = [
-        VarChar("classification", 25),
-        VarChar("symbol", 25, primary_key=True),
-        VarChar("name", 25),
-        VarChar("ISINCode", 25),
-        VarChar("listing_date", 25),
-        VarChar("market_category", 25),
-        VarChar("industry_category", 25),
-        VarChar("CFICode", 25),
-        VarChar("remark", 25),
-    ]
-    table_name = "stock_info"
-
-    def open_spider(self, spider):
+    def open_spider(self, spider: scrapy.Spider):
         """
-        Open the spider and initialize the MariaDB client.
+        Initialize the SQLAlchemy engine and create table.
 
         Parameters:
             spider: The spider instance.
         """
-        table = MariadbTable(self.table_name, self.data_type)
-        info = login_info()
-        self.client = MariadbClient(table, **info)
+        engine = init_engine()
+        StockInfo.metadata.create_all(engine)
+        self.session = Session(engine)
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def process_item(self, item, spider):
+    def process_item(self, item: dict, spider: scrapy.Spider):
         """
-        Process the scraped item and insert it into the MariaDB table.
+        Process the scraped item and insert it into database.
 
         Parameters:
             item: The item to be processed.
@@ -76,88 +139,43 @@ class StockInfoPipeline:
         Returns:
             item: The processed item.
         """
-        self.client.insert(*item.values())
+        obj = StockInfo(**item)
+        result = self.session.get(StockInfo, obj.ISINCode)
+        if not result:
+            self.session.add(obj)
+        else:
+            result.update_attributes(**item)
         self.logger.info(item)
         return item
 
-    def close_spider(self, spider):
+    def close_spider(self, spider: scrapy.Spider):
         """
-        Close MariaDB client connection.
+        Commit transaction and close SQLAlchemy session.
 
         Parameters:
             spider: The spider instance.
         """
-        self.client.close()
-
-
-class DailyTradingRecord:
-    data_type = [
-        SmallInt("symbol", attribute=UNSIGNED, primary_key=True),
-        SmallInt("year", attribute=UNSIGNED, primary_key=True),
-        TinyInt("month", attribute=UNSIGNED, primary_key=True),
-        Text("update_time"),
-    ]
-    table_name = "stock_daily_trading_record"
-
-    @property
-    def table(self):
-        """
-        Return MariadbTable composed of table_name and data_type.
-        """
-        return MariadbTable(self.table_name, self.data_type)
+        self.session.commit()
+        self.session.close()
 
 
 class DailyTradingPipeline:
     """
     A class for processing daily trading information of stock.
-
-    Attributes:
-        data_type (List[DateType]): A list composed of DataType used to
-        represent a SQL table.
-        table_name (str): The name of SQL table.
-        check_data_type (List[DateType]): A list composed of DataType
-        used to represent a SQL table for storing update time.
-        table_name (str): The name of SQL table for storing update time.
     """
 
-    data_type = [
-        SmallInt("symbol", attribute=UNSIGNED, primary_key=True),
-        SmallInt("year", attribute=UNSIGNED, primary_key=True),
-        TinyInt("month", attribute=UNSIGNED, primary_key=True),
-        TinyInt("day", attribute=UNSIGNED, primary_key=True),
-        BigInt("volume", attribute=UNSIGNED),
-        BigInt("value", attribute=UNSIGNED),
-        Decimal("open", 8, 3),
-        Decimal("highest", 8, 3),
-        Decimal("lowest", 8, 3),
-        Decimal("closing", 8, 3),
-        VarChar("delta", 10),
-        Integer("transaction_volume"),
-    ]
-    table_name = "stock_daily_trading"
-
-    def open_spider(self, spider):
+    def open_spider(self, spider: scrapy.Spider):
         """
-        Open the spider and initialize the MariaDB client.
-
-        It will create another table for storing update time.
+        Initialize the SQLAlchemy engine and create table.
 
         Parameters:
             spider: The spider instance.
         """
-        table = MariadbTable(self.table_name, self.data_type)
-        info = login_info()
-        self.client = MariadbClient(table, **info)
-        self.record_client = MariadbClient(
-            DailyTradingRecord().table, self.client.cursor(), **info
-        )
+        self.engine = init_engine()
+        DailyTradingInfo.metadata.create_all(self.engine)
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def commit(self):
-        """Commit to SQL database."""
-        self.client.commit()
-
-    def process_item(self, item, spider):
+    def process_item(self, item: dict, spider: scrapy.Spider):
         """
         Process the scraped item and insert it into the MariaDB table.
 
@@ -170,44 +188,46 @@ class DailyTradingPipeline:
         """
         symbol = item["symbol"]
         data_list = item["data"]
-        fields = item["fields"]
-        for data in data_list:
-            date = data[0]
-            formatted_data = {"symbol": symbol}
-            formatted_data.update(
-                (name, value)
-                for name, value in zip(
-                    ["year", "month", "day"], date.split("/")
+        with Session(self.engine) as session:
+            for data in data_list:
+                obj = DailyTradingInfo(
+                    symbol,
+                    *[
+                        None if value == "--" else value.replace(",", "")
+                        for value in data
+                    ],
                 )
-            )
-            for name, value in zip(fields[1:], data[1:]):
-                if value == "--":
-                    formatted_data[name] = None
+                result = session.get(
+                    DailyTradingInfo, (obj.symbol, obj.timestamp)
+                )
+                if not result:
+                    session.add(obj)
                 else:
-                    formatted_data[name] = value.replace(",", "")
-            self.client.insert(*formatted_data.values())
-        current = datetime.utcnow() + timedelta(hours=8)  # Current taipei time
-        year = int(formatted_data["year"])
-        month = int(formatted_data["month"])
-        if current.year != year or current.month != month:
-            self.record_client.insert(
-                symbol, year, month, current.strftime("%Y/%m/%d/%H:%M")
-            )
-            self.logger.info(f"{symbol} {year}/{month} is done.")
-        else:
-            self.logger.info(
-                f"{symbol} {year}/{month} is current month, "
-                "so we need to update everyday."
-            )
-        self.commit()
+                    result.update_attributes(obj)
+            current = datetime.datetime.now(datetime.UTC) + datetime.timedelta(
+                hours=8
+            )  # Current taipei time
+            date_str = data_list[0][0]
+            year, month, _ = map(int, date_str.split("/"))
+            if current.year != year or current.month != month:
+                obj = DailyTradingRecord(
+                    symbol, datetime.date(year, month, 1), current
+                )
+                result = session.get(
+                    obj.__class__, (obj.symbol, obj.timestamp)
+                )
+                if not result:
+                    session.add(obj)
+                else:
+                    result.update_time = current
+                self.logger.info(f"{symbol} {year}/{month} is done.")
+            else:
+                self.logger.info(
+                    f"{symbol} {year}/{month} is current month, "
+                    "so we need to update once this month concludes."
+                )
+            session.commit()
         return item
 
-    def close_spider(self, spider):
-        """
-        Close MariaDB client connection.
-
-        Parameters:
-            spider: The spider instance.
-        """
-        self.record_client.close()
-        self.client.close()
+    def close_spider(self, spider: scrapy.Spider):
+        pass
